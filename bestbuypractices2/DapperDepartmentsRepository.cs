ec91432bcd48abf8920ffeb085b71cd429991891
bella <theboss@ciaobella.me>
2021-07-13 10:25:31 -0700

    awndi

---
+++ bestbuypractices2/DapperDepartmentsRepository.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using IntroSQL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace bestbuypractices2
+{
+    class DapperDepartmentsRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _connection;
+        //Constructor
+        public DapperDepartmentsRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Department> GetAllDeparments()
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM Departments;");
+        }
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+            new { departmentName = newDepartmentName });
+        }
+
+
+    }
+}
---
+++ bestbuypractices2/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using IntroSQL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bestbuypractices2
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDeparments();
+    }
+}
